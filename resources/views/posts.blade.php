d421b49427d01c9625ec26ed8131766c6048da7f
vico65 <alvicojr65@gmail.com>
2024-11-28 21:04:22 +0700

    Menyelesaikan redesign ui

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,6 @@
 <x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
+   <x-slot:title>{{ $title }}</x-slot:title>
 
-    
-        {{-- <article class="py-8 max-w-screen-md border-b border-gray-300">
-      <a href="/posts/{{ $post['slug'] }}" class="hover:underlineo">
-         <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">{{ $post['title'] }}</h2>
-      </a>
-      
-      <div class=""> By
-         <a class="hover:underline text-base text-gray-500" href="/authors/{{ $post->author->username }}">{{ $post->author->name }} </a>
-         in
-         <a href="/categories/{{ $post->category->slug }}" class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a> | {{ $post->created_at->diffForHumans() }}
-      </div>
-
-      <p class="my-4 font-light">{{ Str::limit($post['body'], 160) }}</p>
-
-      <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
-   </article> --}}
 
    <div class="py-6 mx-auto max-w-screen-xl">
       <div class="grid gap-4 lg:grid-cols-3">
@@ -42,7 +26,7 @@
                            <a class="hover:underline" href="/authors/{{ $post->author->username }}">{{ $post->author->name }} </a>
                         </span>
                      </div>
-                     <a href="#"
+                     <a href="/posts/{{ $post['slug'] }}"
                         class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                         Read more
                         <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
--- tailwind.config.js
+++ tailwind.config.js
@@ -67,6 +67,7 @@ export default {
         }
     },
     plugins: [
-        require("flowbite/plugin")
+        require("flowbite/plugin"),
+        require('flowbite-typography'),
     ],
 };
